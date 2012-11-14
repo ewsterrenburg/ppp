bc0087306120d812645377d03b03faac3711bb4f
Erwin <erwin@Rhinoceros.(none)>
2012-11-14 22:44:44 +0100

    os enri

--- manage.py
+++ manage.py
@@ -22,7 +22,8 @@ for i, arg in enumerate(sys.argv):
 
 # Run Django.
 if __name__ == "__main__":
-    settings_module = "%s.settings" % PROJECT_DIRNAME
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+    #settings_module = "%s.settings" % PROJECT_DIRNAME
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
